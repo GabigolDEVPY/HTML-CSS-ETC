b45c1d77a6dad167e1ae43f103e2147205632ee5
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2025-05-12 23:23:31 -0300

    adicionando últimos arquivos de hoje

--- PROJETOS-1/app/utils/add_functs.py
+++ PROJETOS-1/app/utils/add_functs.py
@@ -4,11 +4,17 @@ from flask import render_template
 def addProduct(data, email):
     data = data
     email = email
-    print(email)
     connection = gc()
     cursor = connection.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM usuarios")
-    dados_bd = cursor.fetchone()
+    cursor.execute("SELECT id FROM usuarios WHERE email = %s", (email,))
+    id_bd = cursor.fetchone()
+    print(id_bd)
+    url = "fsdfjsdkjfkjsdfkds"
+    cursor.execute("""INSERT INTO produtos ( usuario_id, nome, descricao, img_url, tipo, url_reference)
+                    VALUES (%s, %s, %s, %s, %s, %s)
+                """,
+                (int(id_bd['id']), data["name"], data["description"], data["url"], data["type"], url))
+    connection.commit()
 
     return render_template("add.html", mensagem="Produto cadastrado com sucesso")
     
