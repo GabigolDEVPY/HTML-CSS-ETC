253c75baace55acde17e53b75f6d54b31df52eb6
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2025-05-13 21:20:47 -0300

    add

--- PROJETOS-1/app/routes/edit.py
+++ PROJETOS-1/app/routes/edit.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, abort, request, render_template, session
+from ..db.db_connect import get_connection
 
 
 edit_bp = Blueprint('edit', __name__)
@@ -9,20 +10,26 @@ def return_edit():
     if 'email' and 'senha' in session:
         if request.method == "GET":
             return render_template("edit.html")
-        if request.method == "PUT":
-            data = request.form.to_dict()
-            return render_template("edit.html")
     else:
         abort(403)
         
 @edit_bp.route('/return-product', methods=["GET"])
 def return_product():
-    product = ['1']
     product_id = request.args.get('id')
+    # conexao SQL
+    connection = get_connection()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT nome, descricao, img_url, preco, tipo FROM produtos WHERE id = %s",(product_id,))
+    product = cursor.fetchone()
+    print(product)
+    
+    
     # return render_template("edit.html", error="erro")             
     return render_template("edit.html", product=product)   
 
 
 @edit_bp.route('/edit-product', methods=["POST"])
 def edit_product():
+    dados = request.form.to_dict()
+    print(dados)
     return render_template("edit.html")
